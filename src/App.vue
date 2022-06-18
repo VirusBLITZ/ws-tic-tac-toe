de18ef1c0d2a5859c6e6f1cb39ab9fb8eb5949b9
VirusBLITZ <58221423+VirusBLITZ@users.noreply.github.com>
2022-06-18 23:29:07 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+
+const root = document.getElementById('app')
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  root!.style.backgroundColor = '#000000'
+} else {
+  root!.style.backgroundColor = '#fff'
+}
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  height: 100vh;
+}
+</style>
