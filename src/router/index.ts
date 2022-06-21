0a89bdc59c7364a4a2c3e1ffeea185ae29890d43
VirusBLITZ <58221423+VirusBLITZ@users.noreply.github.com>
2022-06-21 22:07:54 +0200

    add loading and join page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,21 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router"
 // import Recent from "../components/recent.vue";
 import Start from "../views/start.vue"
+import Loading from "../views/loading.vue"
 const Play = () => import("../views/play.vue")
 const New = () => import("../views/new.vue")
+const Join = () => import("../views/join.vue")
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "Start",
     component: Start,
+  },
+  {
+    path: "/loading",
+    name: "Loading",
+    component: Loading,
   },
   {
     path: "/play/:id",
@@ -19,10 +26,13 @@ const routes: Array<RouteRecordRaw> = [
     path: "/new/:id",
     name: "New",
     component: New,
-    meta: {
-      id: 0,
-    },
     alias: "/new",
+  },
+  {
+    path: "/join/:id",
+    name: "Join",
+    component: Join,
+    alias: "/join",
   },
   // {
   //   path: "/library",
